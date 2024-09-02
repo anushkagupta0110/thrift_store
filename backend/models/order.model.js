437a5154bfbe98a96cc550aa1a01e21a452a33d3
Anushka <anushkagupta11003@gmail.com>
2024-09-02 22:00:07 +0530

    worked on models

---
+++ backend/models/order.model.js
@@ -0,0 +1,52 @@
+// importing libraries
+import mongoose from "mongoose";
+
+// specifying schema for order
+const OrderSchema = mongoose.Schema(
+    {
+        name:
+        {
+            type: String,
+            require: true
+        },
+        userId:
+        {
+            type: String,
+            require: true
+        },
+        products:
+        {
+            type: Array,
+            require: true
+        },
+        total:
+        {
+            type: Number,
+            require: true
+        },
+        address:
+        {
+            type: String
+        },
+        phone:
+        {
+            type: String
+        },
+        email:
+        {
+            type: String
+        },
+        status:
+        {
+            type: Number,
+            default: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+// exporting order
+const Order = mongoose.model("Order", OrderSchema);
+export default Order;
---
+++ backend/models/product.model.js
@@ -0,0 +1,87 @@
+// importing libraries
+import mongoose from "mongoose";
+
+// specifying schema for product
+const ProductShcema = mongoose.Schema(
+    {
+        title:
+        {
+            type: String,
+            require: true
+        },
+        desc:
+        {
+            type: String,
+            require: true
+        },
+        whatInBox:
+        {
+            type: String
+        },
+        img:
+        {
+            type: String,
+            require: true
+        },
+        video:
+        {
+            type: String
+        },
+        wholesalePrice:
+        {
+            type: Number
+        },
+        wholesaleMinimumQuantity:
+        {
+            type: Number
+        },
+        categories:
+        {
+            type: Array
+        },
+        concern:
+        {
+            type: Array
+        },
+        brand:
+        {
+            type: String
+        },
+        skintype:
+        {
+            type: Array
+        },
+        originalPrice:
+        {
+            type: Number,
+            require: true
+        },
+        discountedPrice:
+        {
+            type: Number
+        },
+        inStock:
+        {
+            type: Boolean,
+            default: true
+        },
+        rating:
+        [
+            {
+                star: {type: Number},
+                name: { type: String },
+                comment: { type: String },
+                postedBy: { type: String },
+            },
+        ],
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+// exporting product
+const Product = mongoose.model("Product", ProductShcema);
+export default Product;
